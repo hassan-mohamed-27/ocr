df1b7d1a5f1cc98ac066380fb432b8df96dbb64a
hassan <hassan.mohamed.shair@gmail.com>
2024-11-23 16:56:45 +0200

    Add files via upload

---
+++ app/services/__init__.py
@@ -0,0 +1,1 @@
+# This file can be left empty or used to set up service modules if needed 
---
+++ app/utils/config.py
@@ -0,0 +1,3 @@
+CREDENTIALS_PATH = 'client_secret.json'
+TOKEN_PATH = 'token.json'
+DOWNLOADS_DIR = 'downloads'
